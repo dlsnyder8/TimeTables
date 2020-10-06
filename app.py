1fdae9f34e859e84229404eec1b8745fc9564c4e
Dylan Snyder <dlsnyder@princeton.edu>
2020-10-06 02:20:42 -0500

    basic redirection testing

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
 from flask import Flask
+from flask import render_template, make_response, request, redirect, url_for
+from CASClient import CASClient
+
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/',methods=['GET'])
+@app.route('/index',methods=['GET'])
+def index():
+    
+    html = render_template('index.html')
+    response = make_response(html)
 
+    return response
+
+
+
+@app.route('/profile',methods=['GET'])
+def profile():
+    return "profile page"
+
+
+
+@app.route('/schedule', methods=['GET'])
+def schedule():
+    return "schedule page"
+
+@app.route('/groupInfo', methods=['GET'])
+def groupInfo():
+    return "Group info"
+
+
+@app.route('/createProfile',methods=['GET'])
+def createProfile():
+    return "Create Profile Page"
 
 
 if __name__ == '__main__':
