f4d36f68c233bfae755f13cc124f927e608fef9a
batyap <batyas@princeton.edu>
2020-10-22 12:12:05 -0400

    added groupid to get_profile_info

--- app.py
+++ app.py
@@ -44,8 +44,8 @@ def profile():
         username = 'test123'
     # is netid = username?
     # get groupid from cookie that takes info from input into index page
-
-    userInfo, notifPrefs = get_profile_info(username)
+    groupid = 1
+    userInfo, notifPrefs = get_profile_info(username, groupid)
     html = render_template('profile.html', firstName = userInfo.firstname, lastName = userInfo.lastname, netid=username, email=userInfo.email, phoneNum=userInfo.phone, phonePref=notifPrefs.emailnotif, emailPref=notifPrefs.textnotif)
     response = make_response(html)
 
