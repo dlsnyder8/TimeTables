0021ad93c6ca5c4865e74101585dde334d414638
batyap <batyas@princeton.edu>
2020-10-22 17:20:19 -0400

    editingprofile

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template, make_response, request, redirect, url_for
 from CASClient import CASClient
-from database import get_profile_info, add_user
+from database import get_profile_info, add_user, update_user
 import os
 from sys import stderr, exit
 import urllib.parse as urlparse
@@ -17,8 +17,6 @@ PROD_ENV = False
 
 app = Flask(__name__)
 app.secret_key = b'\x06)\x8e\xa3BW"\x9d\xcd\x1d5)\xd6\xd1b1'
-
-
 
 
 @app.route('/',methods=['GET'])
@@ -105,6 +103,40 @@ def createProfile():
 
         return redirect(url_for('profile'))
 
+@app.route('/editProfile',methods=['GET','POST'])
+def editProfile():
+    if(PROD_ENV):
+        username = CASClient().authenticate()
+    else:
+        username = 'test123'
+
+    # add error handling if username already exists in database
+
+    groupid = 1
+    userInfo, notifPrefs = get_profile_info(username, groupid)
+    prevfirstName = userInfo.firstname
+    prevlastName = userInfo.lastname
+    prevemail=userInfo.email 
+    prevphoneNum=userInfo.phone
+    prevphonePref=notifPrefs.emailnotif
+    prevemailPref=notifPrefs.textnotif
+
+    if request.method == 'GET':
+        html = render_template('editProfile.html', prevfirstName, prevlastName, prevemail, prevphoneNum, prevphonePref, prevemailPref)
+        response = make_response(html)
+        return response
+
+    else:
+        fname = request.form['fname']
+        lname = request.form['lname']
+
+        email = request.form['email']
+        pnum = request.form['pnumber']
+        #preftext = request.args.get('preftext')
+        #prefemail = request.args.get('prefemail')
+        update_user(fname, lname, username, email, pnum)
+
+        return redirect(url_for('profile'))
 
 if __name__ == '__main__':
     app.run()
